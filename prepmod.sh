f371fd136c303fe4419d54bc7cfcd138d0e0634b
Jens Bodal <jensbodal@gmail.com>
2021-03-19 13:12:22 -0700

    added local clinic

--- prepmod.sh
+++ prepmod.sh
@@ -4,6 +4,7 @@ logtime=$(date "+%Y-%m-%dT%H:%M:%S")
 everett="https://prepmod.doh.wa.gov/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=angel+of+the+winds&clinic_date_eq%5Byear%5D=&clinic_date_eq%5Bmonth%5D=&clinic_date_eq%5Bday%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search"
 evergreen="https://prepmod.doh.wa.gov/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=evergreen+state+fairgrounds&clinic_date_eq%5Byear%5D=&clinic_date_eq%5Bmonth%5D=&clinic_date_eq%5Bday%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search"
 arlington="https://prepmod.doh.wa.gov/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=Arlington+Airport&clinic_date_eq%5Byear%5D=&clinic_date_eq%5Bmonth%5D=&clinic_date_eq%5Bday%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search"
+bodal="https://prepmod.doh.wa.gov/clinic/search?location=98026&search_radius=10+miles&q%5Bvenue_search_name_or_venue_name_i_cont%5D=&clinic_date_eq%5Byear%5D=&clinic_date_eq%5Bmonth%5D=&clinic_date_eq%5Bday%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search"
 
 log() {
   printf "[$logtime] $@\n"
@@ -28,6 +29,7 @@ main() {
 
   log "Time to register!"
   open "$site"
+  echo 'Time to register!' | /usr/local/bin/terminal-notifier -title "Covid Alert! [$sitename]" -subtitle "Did not find text $searchstring" -sound sosumi -open "$site"
 
   return 0;
 }
